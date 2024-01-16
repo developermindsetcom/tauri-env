baef4e3a44988a0b0020b54db0dc1a414b131b7c
Eugene Hauptmann <eugene@reactivelions.com>
2024-01-15 22:00:05 -0500

    added std::env

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    std::env::var(String::from(name)).unwrap_or(String::from(""))
 }
 
 fn main() {

19b1cbdbb5876934887a892fdbd1eef46671533b
Eugene Hauptmann <eugene@reactivelions.com>
2024-01-16 00:35:26 -0500

    added rust dotenv loading

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn get_env(name: &str) -> String {
@@ -10,12 +8,26 @@ fn get_env(name: &str) -> String {
 }
 
 fn main() {
+    dotenv::load().ok();
+
+    if cfg!(debug_assertions) {
+        dotenv::from_filename(".env.development").ok();
+    }else{
+        // dotenv::from_filename("../../.env.production").ok();
+
+        let prod_env = include_str!("../../.env.production");
+        println!("prod_env {}", prod_env);
+        let result = dotenv::from_read(prod_env.as_bytes()).unwrap();
+        result.load();
+    }
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let main = app.handle().get_window("main").unwrap();
-            main.open_devtools();
-            
+        .setup(|_app| {
+            // use tauri::Manager as _;
+            // let main = app.handle().get_window("main").unwrap();
+            // main.open_devtools();
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![get_env])

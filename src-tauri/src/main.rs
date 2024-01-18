9893f3e97450bb95412386485b943bc7e90b8e8c
DeveloperMindset.com <154647374+developermindsetcom@users.noreply.github.com>
2024-01-17 23:47:42 -0500

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,28 +8,30 @@ fn get_env(name: &str) -> String {
 }
 
 fn main() {
-    dotenv::load().ok();
+    // dotenv::load().ok();
 
     if cfg!(debug_assertions) {
-        dotenv::from_filename(".env.development").ok();
+        dotenv::from_filename(".env.development").unwrap().load();
     }else{
-        // dotenv::from_filename("../../.env.production").ok();
+        // loads from the root of the `tauri build` command
+        // dotenv::from_filename(".env.production").unwrap().load();
 
+        // loads inside the source code relative to this file
         let prod_env = include_str!("../../.env.production");
-        println!("prod_env {}", prod_env);
+        // println!("prod_env {}", prod_env);
         let result = dotenv::from_read(prod_env.as_bytes()).unwrap();
         result.load();
     }
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .setup(|_app| {
+        // .setup(|_app| {
             // use tauri::Manager as _;
             // let main = app.handle().get_window("main").unwrap();
             // main.open_devtools();
 
-            Ok(())
-        })
+            // Ok(())
+        // })
         .invoke_handler(tauri::generate_handler![get_env])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
